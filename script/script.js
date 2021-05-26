d8b910f437e16e12bb9dd6d845705f66200d24a3
Brewalan <pengsifan@hotmail.com>
2021-05-26 17:39:58 +0200

    Console game finish

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+console.log("Hello World, type game();");
 
 /* 
 Rock beats scissors
@@ -16,29 +16,36 @@ function randomComputer() {
   return rockPaperScissors[Math.floor(Math.random() * 3)];
 } // Function for random choice
 
-const computerSelect = randomComputer();
+// const pS = prompt("Type your choice");
+// console.log(pS);
 
-const playerSelect = randomComputer();
-
-function playRound(playerSelect, computerSelect) {
-  if (computerSelect === "rock" && playerSelect === "rock") {
-    return "its a draw!";
-  } else if (computerSelect === "paper" && playerSelect === "paper") {
-    return "its a draw!";
-  } else if (computerSelect === "scissors" && playerSelect === "scissors") {
-    return "its a draw!";
-  } else if (computerSelect === "rock" && playerSelect === "paper") {
-    return "you win, paper beats rock!";
-  } else if (computerSelect === "rock" && playerSelect === "scissors") {
-    return "you lose, rock beats scissors!";
-  } else if (computerSelect === "paper" && playerSelect === "rock") {
-    return "you lose, paper beats rock!";
-  } else if (computerSelect === "paper" && playerSelect === "scissors") {
-    return "you win, scissors beats paper!";
-  } else if (computerSelect === "scissors" && playerSelect === "rock") {
-    return "you win, rock beats scissors!";
-  } else if (computerSelect === "scissors" && playerSelect === "paper") {
-    return "you lose, scissors beats paper!";
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    const playerSelect = prompt("Type your choice");
+    const computerSelect = randomComputer();
+    console.log(playRound(playerSelect, computerSelect));
+    function playRound(playerSelect, computerSelect) {
+      console.log(playerSelect.toLowerCase());
+      console.log(computerSelect);
+      if (computerSelect === "rock" && playerSelect === "rock") {
+        return "its a draw!";
+      } else if (computerSelect === "paper" && playerSelect === "paper") {
+        return "its a draw!";
+      } else if (computerSelect === "scissors" && playerSelect === "scissors") {
+        return "its a draw!";
+      } else if (computerSelect === "rock" && playerSelect === "paper") {
+        return "you win, paper beats rock!";
+      } else if (computerSelect === "rock" && playerSelect === "scissors") {
+        return "you lose, rock beats scissors!";
+      } else if (computerSelect === "paper" && playerSelect === "rock") {
+        return "you lose, paper beats rock!";
+      } else if (computerSelect === "paper" && playerSelect === "scissors") {
+        return "you win, scissors beats paper!";
+      } else if (computerSelect === "scissors" && playerSelect === "rock") {
+        return "you win, rock beats scissors!";
+      } else if (computerSelect === "scissors" && playerSelect === "paper") {
+        return "you lose, scissors beats paper!";
+      }
+    }
   }
-}
-console.log(playRound(playerSelect, computerSelect));
+};
